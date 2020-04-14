763d84b359d489950e41bdf78ffa13cd1ccd69e1
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-04-13 19:19:52 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
     //if ($action == NULL) {
       //  $action = filter_input(INPUT_GET, 'action');
        // if ($action == NULL) {
-            $action = 'list_vehicles';
+            //$action = 'list_vehicles';
         //}
     //}
 
